5815c5fe62f7c9d43d96b16df62983558a87b091
Shibani Selvakumar <shibs0910@gmail.com>
2026-09-23 14:09:51 +0530

    Introducing compilation error

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -12,6 +12,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println(MESSAGE);
     }
+    compilation error!
 
     public String getMessage() {
         return MESSAGE;
